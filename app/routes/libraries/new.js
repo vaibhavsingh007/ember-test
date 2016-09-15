3c3e66465086ebc35d6445f58f6c2c870c87ef94
Vaibhav Singh <vaibhav.singh21@yahoo.co.in>
2016-09-15 12:40:26 +0200

    Added Components.

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -18,10 +18,26 @@ export default Ember.Route.extend({
     /* A built-in Ember.js action (event) called willTransition that is
     called when you leave a page (route). In our case, we use this action
     to reset the model if we haven’t saved it in the database yet.*/
-    willTransition() {
+    willTransition(transition) {
       // rollbackAttributes() removes the record from the store
       // if the model 'isNew'
-      this.controller.get('model').rollbackAttributes();
+
+      let model = this.controller.get('model');
+
+      // Check if we have pending changes on the page
+      if (model.get('hasDirtyAttributes')){
+        let confirmation = confirm("Your changes haven't been saved. Would you like to leave the form?");
+
+        if (confirmation)
+        {
+          model.rollbackAttributes();
+        }
+        else
+        {
+          transition.abort();
+        }
+      }
     }
   }
+
 });
